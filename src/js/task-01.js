b011aa2944d8341ea2597af88effd9fe1cd7c960
Halyna Kilbovych <vusochina.galina@gmail.com>
2022-11-12 14:46:47 +0200

    1 task done

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -3,36 +3,21 @@
 // 2. Cтворю змінну кількості категорій 
 // 3. Вивожу в консоль 
 
-
-// const title = categoriesList.forEach((item) =>
-//     console.log(item.firstElementChild)
-//   );
-
-const categoriesList = document.querySelectorAll("li.item"); 
+const categoriesList = document.querySelectorAll("li.item");
 
 console.log(`Number of categories: ${categoriesList.length}`);
 
-const title1 = categoriesList[0].firstElementChild;
-const title2 = categoriesList[1].firstElementChild;
-const title3 = categoriesList[2].firstElementChild;
+// Перший список 
+console.log(`Category: ${categoriesList[0].firstElementChild.textContent}`)
 
-const elementsCount = 
+console.log(`Elements: ${categoriesList[0].lastElementChild.children.length}`)
 
+// Другий список 
+console.log(`Category: ${categoriesList[1].firstElementChild.textContent}`)
 
-console.log(categoriesList)
-console.log(title2)
-console.log(title3)
+console.log(`Elements: ${categoriesList[1].lastElementChild.children.length}`)
 
+// Третій список 
+console.log(`Category: ${categoriesList[2].firstElementChild.textContent}`)
 
-
-
-
-
-
-console.log()
-console.log()
-console.log()
-console.log()
-console.log()
-console.log()
-console.log()
+console.log(`Elements: ${categoriesList[2].lastElementChild.children.length}`)
