03742e22c0771910d51d6c4b97068eda2598152c
Halyna Kilbovych <vusochina.galina@gmail.com>
2022-11-12 14:23:11 +0200

    started 1 task

---
+++ src/js/task-01.js
@@ -0,0 +1,38 @@
+
+// 1. Cтворю змінну для загального списку
+// 2. Cтворю змінну кількості категорій 
+// 3. Вивожу в консоль 
+
+
+// const title = categoriesList.forEach((item) =>
+//     console.log(item.firstElementChild)
+//   );
+
+const categoriesList = document.querySelectorAll("li.item"); 
+
+console.log(`Number of categories: ${categoriesList.length}`);
+
+const title1 = categoriesList[0].firstElementChild;
+const title2 = categoriesList[1].firstElementChild;
+const title3 = categoriesList[2].firstElementChild;
+
+const elementsCount = 
+
+
+console.log(categoriesList)
+console.log(title2)
+console.log(title3)
+
+
+
+
+
+
+
+console.log()
+console.log()
+console.log()
+console.log()
+console.log()
+console.log()
+console.log()
